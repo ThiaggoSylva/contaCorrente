39d6e106fab94b8b8d8e0b3356d3722825f1245c
ThiaggoSylva <thiaggol.sylva@gmail.com>
2026-04-06 16:34:59 -0300

    Abstração da Conta Corrente

--- contaCorrente.ConsoleApp/Program.cs
+++ contaCorrente.ConsoleApp/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // Conta Corrente 1
+        ContaCorrente contaUm = new ContaCorrente();
+        contaUm.numeroIdentificacao = 1;
+        contaUm.titular = "Tiago";
+        contaUm.saldo = 400;
+        contaUm.limiteDebito = 1200;
+
+        // Conta Corrente 2
+        ContaCorrente contaDois = new ContaCorrente();
+        contaDois.numeroIdentificacao = 2;
+        contaDois.titular = "Rech";
+        contaDois.saldo = 12000;
+        contaDois.limiteDebito = 1200;
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"Conta Corrente #{contaUm.numeroIdentificacao} de {contaUm.titular}");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("1 - Saque");
+            Console.WriteLine("2 - Depósito");
+            Console.WriteLine("3 - Transferência");
+            Console.WriteLine("4 - Consulta de Saldo");
+            Console.WriteLine("S - Sair");
+            Console.WriteLine("-------------------------------------");
+            Console.Write("Digite uma opção válida: ");
+            string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+            if (opcaoMenu == "S")
+                break;
+
+            if (opcaoMenu == "1")
+            {
+                contaUm.Sacar();
+            }
+            else if (opcaoMenu == "2")
+            {
+                contaUm.Depositar();
+            }
+            else if (opcaoMenu == "3")
+            {
+                contaUm.TransferirPara(contaDois);
+            }
+            else if (opcaoMenu == "4")
+            {
+                contaUm.ObterSaldo();
+            }
+        }
+    }
+}
